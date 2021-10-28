cbcbf9791b1299bde39845a28c26c46e3ebb7b6f
regoe77 <93295914+regoe77@users.noreply.github.com>
2021-10-28 16:06:25 -0500

    Add files via upload

--- CONFIG.h
+++ CONFIG.h
@@ -37,7 +37,7 @@
 // 'C' source line config statements
 
 // CONFIG
-#pragma config FOSC = INTRCCLK  // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config FOSC = INTRCIO  // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = OFF      // GP3/MCLR pin function select (GP3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
